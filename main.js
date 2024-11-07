c19cfd91de58c554c3366d1d2ca2dbf1b63da313
Caique <asapcaique7@gmail.com>
2024-11-07 09:45:45 -0300

    responsividade no carousel 3d section

--- main.js
+++ main.js
@@ -88,18 +88,41 @@ document.addEventListener('DOMContentLoaded', function () {
   const face = document.createElement('div');
   face.classList.add('carousel__face');
   face.style.transform = `rotateY(${index * 72}deg) translateZ(200px)`;
-
+  face.style.setProperty('--index', index);
+  
   face.innerHTML = `
     <img src="./assets/img/products/${produto.imagem}" alt="${produto.titulo}">
     <h5 class="product-carousel-title">${produto.titulo}</h5>
     <p class="product-carousel-preco">R$ <s>${produto.preco.toFixed(2)}</s></p>
     <p>Desconto: <span class="desconto">R$ ${produto.desconto.toFixed(2)}</span></p>
     <button class="add-cart-btn" data-produto='${JSON.stringify(produto)}'><i class="fa-solid fa-plus"></i></button>
-    <a href="./product/index.html?id=${produto.id}" class="btn btn-success mt-2">Ver Mais</a>
+    <a href="./product/index.html?id=${produto.id}" class="product-carousel-preco-btn btn btn-success mt-2">Ver Mais</a>
   `;
 
   carousel.appendChild(face);
 });
+
+
+function createCard(product) {
+    return `
+      <div class="card"> <div class="first">
+            <span class="discount">NEW</span>
+          </div>
+        <div class="image-container">
+          <img src="./assets/img/products/${product.imagem}" alt="${product.titulo}" class="thumbnail-image">
+        </div>
+        <div class="dress-name">${product.titulo}</div>
+        <div class="new-price">R$ ${product.desconto}</div>
+        <div class="buy add-cart-btn" data-produto='${JSON.stringify(product)}'>Compre agora</div>
+      </div>
+    `;
+  }
+
+  // Adiciona os últimos 4 produtos ao container
+  const container = document.querySelector('.container-new-cards');
+  produtos.slice(-4).forEach(product => {
+    container.innerHTML += createCard(product);
+  });
 
 document.querySelectorAll('.add-cart-btn').forEach(button => {
     button.addEventListener('click', () => {
@@ -133,24 +156,4 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    function createCard(product) {
-        return `
-          <div class="card"> <div class="first">
-                <span class="discount">NEW</span>
-              </div>
-            <div class="image-container">
-              <img src="./assets/img/products/${product.imagem}" alt="${product.titulo}" class="thumbnail-image">
-            </div>
-            <div class="dress-name">${product.titulo}</div>
-            <div class="new-price">R$ ${product.desconto}</div>
-            <div class="buy add-cart-btn" data-produto='${JSON.stringify(product)}'>Compre agora</div>
-          </div>
-        `;
-      }
-    
-      // Adiciona os últimos 4 produtos ao container
-      const container = document.querySelector('.container-new-cards');
-      produtos.slice(-4).forEach(product => {
-        container.innerHTML += createCard(product);
-      });
 });
